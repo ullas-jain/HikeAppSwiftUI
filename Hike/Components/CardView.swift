67ae1670ba46e0c4070bf0e6b4d385bc4e4ac9df
ullas-jain <jainullas1994@gmail.com>
2023-09-12 11:48:50 +0530

    Adds a Header and CustomButton to the CardView

--- Hike/Components/CardView.swift
+++ Hike/Components/CardView.swift
@@ -11,20 +11,53 @@ struct CardView: View {
     var body: some View {
         ZStack {
             CustomBackgroundView()
-            ZStack {
-                Circle()
-                    .fill(
-                        LinearGradient(colors: [
-                            Color.customIndigoMedium,
-                            Color.customSalmonLight,
-                        ], startPoint: .topLeading, endPoint: .bottomTrailing)
-                    )
-                    .frame(width: 256, height: 256)
+            VStack {
+                // MARK: - HEADER
 
-                Image("image-1")
-                    .resizable()
-                    .scaledToFit()
-            }
+                VStack(alignment: .leading) {
+                    HStack {
+                        Text("Hiking")
+                            .fontWeight(.black)
+                            .font(.system(size: 52))
+                            .foregroundStyle(
+                                LinearGradient(colors: [.customGrayLight, .customGrayMedium], startPoint: .top, endPoint: .bottom)
+                            )
+
+                        Spacer()
+
+                        Button {
+                            // ACTION show a sheet: TODO
+                        } label: {
+                            CustomButtonView()
+                        }
+                    }
+
+                    Text("Fun and enjoyable outdoor activity for family and friends")
+                        .multilineTextAlignment(.leading)
+                        .italic()
+                        .foregroundColor(.customGrayMedium)
+                } // HEADER
+                .padding(.horizontal, 30)
+
+                // MARK: - MAIN CONTENT
+
+                ZStack {
+                    Circle()
+                        .fill(
+                            LinearGradient(colors: [
+                                Color.customIndigoMedium,
+                                Color.customSalmonLight,
+                            ], startPoint: .topLeading, endPoint: .bottomTrailing)
+                        )
+                        .frame(width: 256, height: 256)
+
+                    Image("image-1")
+                        .resizable()
+                        .scaledToFit()
+                }
+
+                // MARK: - FOOTER
+            } // VSTACK
         } //: CARD
         .frame(width: 320, height: 570)
     }
