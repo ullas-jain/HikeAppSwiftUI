4c84848b75cd7219c484be9993bbf168df03c9c9
ullas-jain <jainullas1994@gmail.com>
2023-09-12 11:35:36 +0530

    Use CardView in the main view

--- Hike/Components/CardView.swift
+++ Hike/Components/CardView.swift
@@ -10,19 +10,23 @@ import SwiftUI
 struct CardView: View {
     var body: some View {
         ZStack {
-            Circle()
-                .fill(
-                    LinearGradient(colors: [
-                        Color.customIndigoMedium,
-                        Color.customSalmonLight,
-                    ], startPoint: .topLeading, endPoint: .bottomTrailing)
-                )
-                .frame(width: 256, height: 256)
+            CustomBackgroundView()
+            ZStack {
+                Circle()
+                    .fill(
+                        LinearGradient(colors: [
+                            Color.customIndigoMedium,
+                            Color.customSalmonLight,
+                        ], startPoint: .topLeading, endPoint: .bottomTrailing)
+                    )
+                    .frame(width: 256, height: 256)
 
-            Image("image-1")
-                .resizable()
-                .scaledToFit()
-        }
+                Image("image-1")
+                    .resizable()
+                    .scaledToFit()
+            }
+        } //: CARD
+        .frame(width: 320, height: 570)
     }
 }
 
