dd07866165c0ee92ce38c604a914da1a0123fa6a
ullas-jain <jainullas1994@gmail.com>
2023-09-13 11:45:10 +0530

    Adds presentationDetents and presentationIndicator to the SettingsSheet

--- Hike/Components/CardView.swift
+++ Hike/Components/CardView.swift
@@ -48,6 +48,8 @@ struct CardView: View {
                         }
                         .sheet(isPresented: $isShowingSheet) {
                             SettingsView()
+                                .presentationDragIndicator(.visible)
+                                .presentationDetents([.medium, .large])
                         }
                     }
 
